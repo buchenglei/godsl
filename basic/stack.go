e5077de4774509e88030f574e07754f7ecfdbd75
buchenglei <mybc724@163.com>
2015-08-22 10:03:01 +0800

    完善注释，更新项目进度

--- basic/stack.go
+++ basic/stack.go
@@ -6,10 +6,11 @@ import (
 	"fmt"
 )
 
+// 声明栈的结构
 type Stack struct {
-	capacity int
-	top      int
-	array    []interface{}
+	capacity int           // 栈的容量
+	top      int           // 指向栈顶的指针,index从0开始
+	array    []interface{} // 用于保存数据的数组
 }
 
 func NewStack(capacity int) Stack {
@@ -34,6 +35,8 @@ func (s *Stack) IsEmpty() bool {
 	return s.top == -1
 }
 
+// 由于top指针的index从0开始
+// 所以当栈满时top的值就等于capacity - 1
 func (s *Stack) IsFull() bool {
 	return (s.top + 1) == s.capacity
 }
@@ -57,6 +60,7 @@ func (s *Stack) Pop() error {
 	return nil
 }
 
+// 弹出栈顶的值并返回
 func (s *Stack) TopAndPop() (interface{}, error) {
 	if s.IsEmpty() {
 		return nil, fmt.Errorf("The stack is empty!")
@@ -67,6 +71,7 @@ func (s *Stack) TopAndPop() (interface{}, error) {
 	return data, nil
 }
 
+// 将栈置为空， top = -1
 func (s *Stack) PopAll() error {
 	if s.IsEmpty() {
 		return fmt.Errorf("The stack is empty!")
@@ -76,6 +81,12 @@ func (s *Stack) PopAll() error {
 	return nil
 }
 
+// 增加当前栈的容量
+// 5(正数) 将栈的容量设为: 原来的值 + 5
+// -5(负数) 将站的容量设为: 原来的值 - 5
+// 有一点需要注意的是：
+// 减少当前栈的容量，若新栈的容量不足以保存之前栈的所有值
+// 则丢弃原栈多出去的值丢弃，并将top指向新栈的栈顶
 func (s *Stack) AddCapacity(num int) error {
 	if s.capacity+num <= 0 {
 		return fmt.Errorf("this value will make stack's capacity less than 0,It would be meaningless")
